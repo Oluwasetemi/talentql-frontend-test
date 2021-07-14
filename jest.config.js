dc13aa1e5f0da148a98ad66beee6a436e2c8157f
Ojo Oluwasetemi <setemiojo@gmail.com>
2021-07-14 01:23:29 +0100

    🐛 FIX: clean up coverage error WIP

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,20 @@ module.exports = {
 	roots: ['<rootDir>/src'],
 	preset: 'ts-jest',
 	// testEnvironment: 'js-dom',
+	collectCoverageFrom: ['handlers/**/*.ts', 'src/*'],
+	coveragePathIgnorePatterns: ['<rootDir>/node_modules', 'src/filter.json'],
+	coverageReporters: ['json', 'text', 'lcov', 'clover'],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
 	transform: {
 		'^.+\\.svg$': '<rootDir>/svg-transform.js',
-		'^.+\\.tsx?$': 'ts-jest',
+		'.ts': '<rootDir>/node_modules/ts-jest/preprocessor.js',
 	},
 	testEnvironment: 'jest-environment-jsdom-sixteen',
 	watchPlugins: [
